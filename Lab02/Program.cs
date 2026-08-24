3926e902036938a08d60ef9e48b7a14e1a5d7df6
HulaHoopZ2478 <krwtus@gmail.com>
2026-08-24 10:47:26 +0700

    AddHeader

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,4 +1,12 @@
-﻿namespace Lab02
+﻿/*
+ * Student ID : 1670705969
+ * Name       : Korawit Nakthong
+ * Section    : 129A
+ * No.        :
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+namespace Lab02
 {
     internal class Program
     {
