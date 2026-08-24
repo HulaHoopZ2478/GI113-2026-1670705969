5c793ad8edc8d58d1af20dccda69319aa5ef927a
HulaHoopZ2478 <krwtus@gmail.com>
2026-08-24 10:51:48 +0700

    AddEXcode

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -12,7 +12,22 @@ namespace Lab02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("----- Boss STATUS: INITAIL -----");
+            Console.WriteLine();
+
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP Percent: {hpPercent}%");
+            Console.WriteLine();
+            Console.WriteLine("Kirin takes 60 damage!");
         }
     }
 }
