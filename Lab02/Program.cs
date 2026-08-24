8c3295e0a84eaffc70397b6448277ae6d9c5dfcc
HulaHoopZ2478 <krwtus@gmail.com>
2026-08-24 11:15:59 +0700

    AddinfoBoss

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -20,14 +20,28 @@ namespace Lab02
             float attackPower = 42.5f;
             double critMultiplier = 1.75;
             bool isBoss = true;
-
-            Console.WriteLine("----- Boss STATUS: INITAIL -----");
-            Console.WriteLine();
-
             int hpPercent = currentHp * 100 / maxHp;
+
+            Console.WriteLine("===== Boss STATUS: INITAIL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            Console.WriteLine($"Attack Power: {attackPower}");
+            Console.WriteLine($"Crit Multiplier: {critMultiplier}");
+            Console.WriteLine($"IS BOSS: {isBoss}");
+            Console.WriteLine();
             Console.WriteLine($"HP Percent: {hpPercent}%");
             Console.WriteLine();
+
             Console.WriteLine("Kirin takes 60 damage!");
+            currentHp = currentHp - 60;
+
+            Console.WriteLine("===== Boss STATUS: AFTER DAMAGE =====");
+            int hpPercentTdmg = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            Console.WriteLine($"HP Percent: {hpPercentTdmg}%");
+            Console.WriteLine();
         }
     }
 }
