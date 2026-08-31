9cb73621c7f7a24fa59da64db43d8e5abac5c697
HulaHoopZ2478 <korawit.nakt@bumail.net>
2026-08-31 11:09:41 +0700

    Add Lab3

---
+++ Lab3/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Lab3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			const int Maxlevel = 10;
+
+			var bossName = "Kirin";
+			var rank = 'S';
+			int level = 7;
+			int maxHp = 240;
+			int currentHp = 115;
+			float attackPower = 42.5f;
+			double critMultiplier = 1.75;
+			bool isBoss = true;
+
+			Console.WriteLine($"===== KIRIN SAVE CONVERTER=====\nName: {bossName}\nRank: {rank}\nLevel: {level}/{Maxlevel}\nHP: {currentHp} / {maxHp}\nAttack Power: {attackPower}\nCrit Multiplier: {critMultiplier}\nIs Boss: {isBoss}");
+
+			Console.WriteLine("\n----- Implicit Conversion: HP as double -----");
+			double currentHpDouble = currentHp;
+			Console.WriteLine($"HP (double): {currentHpDouble}");
+
+			Console.WriteLine("\n----- Exct HP Percent (no integer truncation) -----");
+			double hpPercent = currentHpDouble * 100 / maxHp;
+			Console.WriteLine($"Hp Percent (exact) {hpPercent}%");
+
+			Console.WriteLine("\n----- Explicit Cast: Attack Power -> Display int -----");
+			int attackINT = (int)attackPower;
+			Console.WriteLine($"Attack Power (int cast): {attackINT}");
+
+			Console.WriteLine("\n----- Cast vs Convert: Crit Multiplier -----");
+			int critCast = (int)critMultiplier;
+			Console.WriteLine($"Crit Multiplier (int cast): {critCast}");
+
+			int critConvert = Convert.ToInt32(critMultiplier);
+			Console.WriteLine($"Crit Multiplier (Convert rounded): {critConvert}");
+		}
+	}
+}
