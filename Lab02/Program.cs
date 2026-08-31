87efcdbcf4ffe7653f59a381f80226b613329167
HulaHoopZ2478 <korawit.nakt@bumail.net>
2026-08-31 09:28:50 +0700

    Add Character

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -52,9 +52,80 @@ namespace Lab02
             Console.WriteLine($"HP Percent: {hpPercentTdmg}%");
             Console.WriteLine();
 
+			Name = "Fatalis";
+			rank = 'S';
+			level = 10;
+			maxHp = 500;
+			currentHp = 267;
+			attackPower = 83.5f;
+			critMultiplier = 2.00;
+			isBoss = true;
+			hpPercent = currentHp * 100 / maxHp;
+			Console.WriteLine("===== Boss STATUS: INITAIL =====");
+			Console.WriteLine($"Name: {Name}");
+			Console.WriteLine($"Rank: {rank}");
+			Console.WriteLine($"Level: {level}");
+			Console.WriteLine($"HP: {currentHp} / {maxHp}");
+			Console.WriteLine($"Attack Power: {attackPower}");
+			Console.WriteLine($"Crit Multiplier: {critMultiplier}");
+			Console.WriteLine($"IS BOSS: {isBoss}");
+			Console.WriteLine();
+			Console.WriteLine($"HP Percent: {hpPercent}%");
+			Console.WriteLine();
 
+			Name = "Lycan";
+			rank = 'D';
+			level = 2;
+			maxHp = 100;
+			currentHp = 78;
+			attackPower = 24.5f;
+			critMultiplier = 1.00;
+			isBoss = false;
+			hpPercent = currentHp * 100 / maxHp;
+			Console.WriteLine("=== Player ===");
+			Console.WriteLine($"Name: {Name}");
+			Console.WriteLine($"Rank: {rank}");
+			Console.WriteLine($"Level: {level}");
+			Console.WriteLine($"HP: {currentHp} / {maxHp}");
+			Console.WriteLine($"Attack Power: {attackPower}");
+			Console.WriteLine($"Crit Multiplier: {critMultiplier}");
+			Console.WriteLine($"IS BOSS: {isBoss}");
+			Console.WriteLine();
+			Console.WriteLine($"HP Percent: {hpPercent}%");
+			Console.WriteLine();
 
-        }
+			Name = "Norma";
+			rank = 'C';
+			level = 4;
+			maxHp = 120;
+			currentHp = 100;
+			attackPower = 32.5f;
+			critMultiplier = 1.00;
+			isBoss = false;
+			hpPercent = currentHp * 100 / maxHp;
+			Console.WriteLine("=== Player ===");
+			Console.WriteLine($"Name: {Name}");
+			Console.WriteLine($"Rank: {rank}");
+			Console.WriteLine($"Level: {level}");
+			Console.WriteLine($"HP: {currentHp} / {maxHp}");
+			Console.WriteLine($"Attack Power: {attackPower}");
+			Console.WriteLine($"Crit Multiplier: {critMultiplier}");
+			Console.WriteLine($"IS BOSS: {isBoss}");
+			Console.WriteLine();
+			Console.WriteLine($"HP Percent: {hpPercent}%");
+			Console.WriteLine();
+
+			Name = "Nergigante";
+			rank = 'S';
+			level = 9;
+			maxHp = 360;
+			currentHp = 270;
+			attackPower = 61.5f;
+			critMultiplier = 1.75;
+			isBoss = false;
+			hpPercent = currentHp * 100 / maxHp;
+			Console.WriteLine($"\n===== Boss STATUS: INITAIL =====\nName: {Name}\nRank: {rank}\nLevel: {level}\nHp: {currentHp} / {maxHp}\nAttack Power: {attackPower}\nCrit Multiplier: {critMultiplier}\nIS BOSS: {isBoss}\n\nHP Percent {hpPercent}%\n");
+		}
 
     }
 }
